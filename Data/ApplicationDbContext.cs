db9ff02b42a843a6f50d81a3d4c5a33961bb50c3
Jeremy Persing <jpersing1999@gmail.com>
2021-01-23 17:49:34 -0800

    C# asp.net tutorial

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using jokes_tutorial.Models;
+
+namespace jokes_tutorial.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<jokes_tutorial.Models.Joke> Joke { get; set; }
+    }
+}
